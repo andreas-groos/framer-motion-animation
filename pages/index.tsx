ba50145b3bcc39e74c302f478f06262234bebac7
Andreas Groos <andreas.groos1@gmail.com>
2021-10-27 08:07:31 -0700

    multiple useSpring() values

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,39 +11,33 @@ type El = {
 
 const Home: NextPage = () => {
   const [el, setEl] = useState<El>({ cx: 200, cy: 200, r: 100 });
-  const cx = useMotionValue(100);
+  const cx = useMotionValue(el.cx);
   const sx = useSpring(cx);
+  const cy = useMotionValue(el.cy);
+  const sy = useSpring(cy);
+  const r = useMotionValue(el.r);
+  const sr = useSpring(r);
+  const strokeWidth = useMotionValue(0);
+  const sstrokeWidth = useSpring(strokeWidth);
 
-  const [clicked, setClicked] = useState(false);
-
-  const variants = {
-    initial: {
-      cx: el.cx,
-      cy: el.cy,
-      r: el.r,
-      fill: "teal",
-      strokeWidth: 0,
-    },
-    final: {
-      cx: 70,
-      cy: 70,
-      r: 70,
-      strokeWidth: 2,
-      stroke: "red",
-    },
-  };
-
+  console.log("cx", cx);
+  console.log("sx", sx);
   return (
     <div id="app">
       <svg>
         <motion.circle
           onClick={() => {
             cx.set(cx.get() + 100);
+            cy.set(cx.get() + 200);
+            r.set(r.get() + 200);
+            strokeWidth.set(20);
           }}
           fill="teal"
           cx={sx}
-          cy={el.cy}
-          r={el.r}
+          cy={sy}
+          r={sr}
+          strokeWidth={sstrokeWidth}
+          stroke="red"
           transition={{
             duration: 0.8,
           }}
